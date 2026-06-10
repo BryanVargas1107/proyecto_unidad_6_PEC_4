1c86da6712e545be6535b38ece4bf9f8c2e21d7d
Bryan Vargas Sanchez <bryan.vargas.s@hotmail.com>
2026-06-10 13:40:01 +0200

    feat: capptura ejercicio 9 - pylint 10/10 en todo el proyecto

--- src/config.py
+++ src/config.py
@@ -6,5 +6,5 @@ al autor y registrar el momento de generación de los ficheros.
 """
 from datetime import datetime
 
-nom_alumne: str = "Bryan Vargas"
+nom_alumne: str = "Bryan Vargas" # pylint: disable=invalid-name
 date_time: str = datetime.now().strftime('%Y%m%d_%H%M%S')
